432c71accf5f52cb849422a9b88d8ee0160ad6f7
Ilya Paymushkin <ipaymushkin76@gmail.com>
2024-03-02 11:27:01 +0300

    2024_03_02

--- 2024_02_17/code.js
+++ 2024_02_17/code.js
@@ -3,13 +3,39 @@ document.addEventListener("DOMContentLoaded", () => {
 	const eyeButton = document.querySelector(eyeButtonClass);
 	const passwordTextField = document.querySelector('#textPassword');
 	// const passwordTextField = document.getElementById('textPassword');
-	eyeButton.addEventListener('click', () => {
+	eyeButton && eyeButton.addEventListener('click', () => {
 		if (passwordTextField.type === 'password') {
 			passwordTextField.type = 'text';
 		} else {
 			passwordTextField.type = 'password';
 		}
 	});
+
+	const checkbox = document.getElementById('checkbox');
+	const checkboxBox = document.querySelector('.checkboxArrowWrapper');
+	const checkboxImage = document.getElementById('checkboxImage');
+	if (checkbox) {
+		checkbox.addEventListener('click', () => {
+			if (checkboxBox.classList.contains('checkboxArrowActive')) {
+				checkboxBox.classList.remove('checkboxArrowActive');
+				checkboxImage.style.display = 'none';
+			} else {
+				checkboxBox.classList.add('checkboxArrowActive');	
+				checkboxImage.style.display = 'block';
+			}
+		});
+	}
+
+	const linkLabel = document.getElementById('linkLabel');
+	
+	linkLabel && linkLabel.addEventListener('click', () => {
+		window.open("https://www.google.ru/", "_blank")
+	})
+
+	const successButton = document.getElementById('successButton');
+	successButton && successButton.addEventListener('click', () => {
+		setTimeout(() => window.location.href = "success.html", 1000);
+	})
 });
 
 /*
